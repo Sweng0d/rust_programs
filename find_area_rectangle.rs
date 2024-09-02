b538c8872f6ef9fde99ef2c46d3ac076b70e5235
Bruno Filetti <101097089+Sweng0d@users.noreply.github.com>
2024-09-02 18:25:56 -0300

    Update find_area_rectangle.rs

--- find_area_rectangle.rs
+++ find_area_rectangle.rs
@@ -25,3 +25,24 @@ fn main () {
 fn area(dimensions: (u32, u32)) -> u32 {
     dimensions.0 * dimensions.1
 }
+
+
+But we can make it even more clear:
+
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn main () {
+    let react1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!("The area of the rectangle is {} square pixels", area(&react1));
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
