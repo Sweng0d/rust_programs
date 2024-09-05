9e7b5b6ca47f377ac48b0ae17cd1256828db4726
Bruno Filetti <101097089+Sweng0d@users.noreply.github.com>
2024-09-05 14:41:54 -0300

    Update find_area_rectangle.rs

--- find_area_rectangle.rs
+++ find_area_rectangle.rs
@@ -46,3 +46,23 @@ fn main () {
 fn area(rectangle: &Rectangle) -> u32 {
     rectangle.width * rectangle.height
 }
+
+
+
+
+Another kind of code:
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!("rect1 is {rect1:?}");
+}
