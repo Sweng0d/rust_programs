a78a20d1d33fe707312a0c8caaab3796e717799c
Bruno Filetti <101097089+Sweng0d@users.noreply.github.com>
2024-09-05 14:44:45 -0300

    Update find_area_rectangle.rs

--- find_area_rectangle.rs
+++ find_area_rectangle.rs
@@ -66,3 +66,22 @@ fn main() {
 
     println!("rect1 is {rect1:?}");
 }
+
+
+More completed:
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn main() {
+    let scale = 2;
+    let rect1 = Rectangle {
+        width: dbg!(30*scale),
+        height: 50,
+    };
+
+    println!("rect1 is {rect1:?}");
+}
