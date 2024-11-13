0299392b75f0350e8ca4b2427f1e454b526b88c3
Bruno Filetti <101097089+Sweng0d@users.noreply.github.com>
2024-11-12 23:38:42 -0300

    Update tests.rs

--- tests.rs
+++ tests.rs
@@ -1,2 +1,40 @@
-my tests
+pub struct SavingsAccount {
+    balance: i32,
 
+}
+
+impl SavingsAccount {
+    pub fn new() -> SavingsAccount{
+        SavingsAccount{
+            balance: 0,
+        }
+    }
+
+    pub fn get_balance(&self) -> i32 {
+        self.balance
+    }
+
+    pub fn deposit(&mut self, amount: i32) {
+        self.balance += amount
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_have_a_starting_balance_of_zero() {
+        let account: SavingsAccount = SavingsAccount::new();
+        assert_eq!(account.get_balance(), 0);
+    }
+
+    #[test]
+    fn deposit_is_right() {
+        let mut account = SavingsAccount::new();
+        account.deposit(150);
+        assert_eq!(account.get_balance(), 150);
+        assert_ne!(account.get_balance(), 0);
+        assert!(account.get_balance() == 150)
+    }
+}
