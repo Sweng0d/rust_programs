e049915c6e224279d994510b3d353eafe78e7301
Bruno Filetti <101097089+Sweng0d@users.noreply.github.com>
2024-11-13 00:18:12 -0300

    Update tests.rs

--- tests.rs
+++ tests.rs
@@ -1,20 +1,68 @@
-error[E0573]: expected type, found module `self`
-PS C:\Users\achil\OneDrive\Área de Trabalho\projects\bank> cargo test
-   Compiling bank v0.1.0 (C:\Users\achil\OneDrive\Área de Trabalho\projects\bank)
-error[E0573]: expected type, found module `self`
-  --> src\lib.rs:21:33
-   |
-21 |     pub fn transfer (from: &mut self, to: &mut self, amount: i32) {
-   |                                 ^^^^ help: a self type with a similar name exists (notice the capitalization): `Self`
+pub struct SavingsAccount {
+    balance: i32,
 
-error[E0573]: expected type, found module `self`
-  --> src\lib.rs:21:48
-   |
-21 |     pub fn transfer (from: &mut self, to: &mut self, amount: i32) {
-   |                                                ^^^^ help: a self type with a similar name exists (notice the capitalization): `Self`
+}
 
-For more information about this error, try `rustc --explain E0573`.
-error: could not compile `bank` (lib) due to 2 previous errors
-warning: build failed, waiting for other jobs to finish...
-error: could not compile `bank` (lib test) due to 2 previous errors
-PS C:\Users\achil\OneDrive\Área de Trabalho\projects\bank>
+impl SavingsAccount {
+    pub fn new() -> SavingsAccount{
+        SavingsAccount{
+            balance: 0,
+        }
+    }
+
+    pub fn get_balance(&self) -> i32 {
+        self.balance
+    }
+
+    pub fn deposit(&mut self, amount: i32) {
+        if amount < 0 {
+            panic!("Cannot deposit a negative value");
+        }
+        self.balance += amount
+    }
+
+    pub fn transfer (from: &mut Self, to: &mut Self, amount: i32) {
+        from.balance -= amount;
+        to.balance += amount;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_have_a_starting_balance_of_zero() {
+        let account: SavingsAccount = SavingsAccount::new();
+        assert_eq!(account.get_balance(), 0);
+    }
+
+    #[test]
+    fn deposit_is_right() {
+        let mut account = SavingsAccount::new();
+        account.deposit(150);
+        assert_eq!(account.get_balance(), 150);
+        assert_ne!(account.get_balance(), 0);
+        assert!(account.get_balance() == 150)
+    }
+
+    #[test]
+    fn transfer_working() {
+        let mut account1 = SavingsAccount::new();
+        let mut account2 = SavingsAccount::new();
+        account1.deposit(200);
+        assert_eq!(account1.get_balance(), 200);
+
+        SavingsAccount::transfer(&mut account1, &mut account2, 50);
+        assert_eq!(account1.get_balance(), 150);
+        assert_eq!(account2.get_balance(), 50);
+    }
+
+    #[test]
+    #[should_panic]
+    fn should_panic_if_deposit_is_negative() {
+        let mut account1 = SavingsAccount::new();
+        account1.deposit(-10);
+
+    }
+}
